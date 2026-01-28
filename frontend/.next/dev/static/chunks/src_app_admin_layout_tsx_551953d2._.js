067ba212a81569949fbcccdefbdfc9de3870383e
dgimenez.developer <dgimenez.developer@gmail.com>
2026-01-28 20:23:13 -0300

    la carpeta .next ya está correctamente ignorada en el archivo .gitignore y eliminada del control de versiones de git. No volverá a subirse a GitHub ni a ser rastreada por git.

--- frontend/.next/dev/static/chunks/src_app_admin_layout_tsx_551953d2._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_330251f3._.js",
-  "static/chunks/src_app_admin_layout_tsx_9b586b61._.js"
-],
-    source: "dynamic"
-});
